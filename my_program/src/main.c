c9dd05f7840b935ab60b94b3040d4d4232f81227
kirpich <yuliakirp@mail.ru>
2025-10-23 09:32:12 +0000

    da

---
+++ my_program/src/main.c
@@ -0,0 +1,6 @@
+#include "my_lib.h"
+
+int main() {
+    print_hello_message();
+    return 0;
+}
---
+++ my_program/src/my_lib.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "my_lib.h"
+
+void print_hello_message() {
+    printf("9ис-495к, Кирпичникова.\n");
+}
